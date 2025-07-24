0481fa46ef18433eb0bea818b3db3c8e5bfb21d2
Ojasva Tomar <ojasvatomar47@gmail.com>
2025-07-25 01:04:31 +0530

    Build fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -42,7 +42,7 @@ export default function ContactPage() {
           >
             <input type="hidden" name="form-name" value="contact" />
             <p className="hidden">
-              <label>Don&apos;t fill this out if you're human: <input name="bot-field" /></label>
+              <label>Don&apos;t fill this out if you&apos;re human: <input name="bot-field" /></label>
             </p>
 
             <div>
